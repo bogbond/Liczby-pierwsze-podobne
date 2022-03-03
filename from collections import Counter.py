f720156500f4690fb1e5044c8e5ca0677f971ad5
Bohdan Bondar <43843880+bogbond@users.noreply.github.com>
2022-03-03 22:27:38 +0100

    optim

--- from collections import Counter.py
+++ from collections import Counter.py
@@ -1,13 +1,40 @@
 from collections import Counter
+import math
 primes = []
-for target_num in range(1, 10000):
-  is_prime = True
-  for i in range(2, target_num):
-    if target_num % i == 0:
-      is_prime = False
-  if is_prime:
-    primes.append(target_num)
+# for target_num in range(10000, 123456789):
+#   is_prime = True
+#   for i in range(2, int(math.sqrt(target_num))):
+#     if target_num % i == 0:
+#       is_prime = False
+#   if is_prime:
+#     primes.append(target_num)
 
+
+n = 123456789
+a = [0] * n  
+for i in range(n):  
+    a[i] = i  
+ 
+a[1] = 0
+ 
+m = 2  
+while m < n:  
+    if a[m] != 0:  
+        j = m * 2  
+        while j < n:
+            a[j] = 0  
+            j = j + m  
+    m += 1
+ 
+
+b = []
+for i in a:
+    if a[i] != 0:
+        b.append(a[i])
+ 
+del a
+#print(b)
+primes = b
 #print(primes)
 #dick={a:primes[a] for a in range(len(primes))}
 #print(dick)
@@ -27,7 +54,7 @@ for a in range(len(primes)):
 values = dick.values()
 counter = Counter(values)
 
-print(dict(counter))
+#print(dict(counter))
 max_val = max(dict(counter).values())
 
 print(max_val)
